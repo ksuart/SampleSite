158b3cb89b4d89c8505437062184438c7dcf193b
koichi-inoue <inoue.ko@gmail.com>
2017-11-21 15:17:59 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,3 +1,28 @@
 function myFunction( col ){
-    ABC.style.backgroundColor = col;
+  var obj = document.getElementById('ABC');
+  obj.style.backgroundColor = col;
 }
+
+$( function(){
+
+  var win = $(window).width();
+  if(win > 640){
+    $("#lists").show();
+  }else{
+    $("#lists").hide();
+  }
+
+  $("#button").click( function () {
+    $("#lists").slideToggle();
+  });
+
+  $(window).on("resize",function(){
+	var win = $(window).width();
+	if(win > 640){
+	  $("#lists").show();
+	}else{
+      $("#lists").hide();
+    }
+  });
+
+});
